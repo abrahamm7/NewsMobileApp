b8357dd0cca2c7518f25342daef940e77a614fe5
Abraham Morillo <abrahammorillo777@gmail.com>
2020-10-16 00:11:04 -0400

    Adding splashscreen and logo

--- NewsAppMobile/NewsAppMobile.Android/MainActivity.cs
+++ NewsAppMobile/NewsAppMobile.Android/MainActivity.cs
@@ -13,14 +13,14 @@ using Plugin.CurrentActivity;
 
 namespace NewsAppMobile.Droid
 {
-    [Activity(Label = "News App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "News App", Icon = "@mipmap/icons8_newspaperyellow", Theme = "@style/MyTheme.Splash", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Forms.Forms.SetFlags(new string[] { "CollectionView_Experimental", "Expander_Experimental", "RadioButton_Experimental", "SwipeView_Experimental" });            
